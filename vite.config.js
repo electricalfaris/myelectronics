b42c255614254d2b1173deadceead98ff6ea3bb4
electricalfaris <electricalfaris@gmail.com>
2024-03-08 20:04:10 +0300

    finished

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/myelectronics/",
   plugins: [
+    react(),
     VitePWA({
       registerType: "propmpt",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
@@ -35,4 +34,5 @@ export default defineConfig({
       },
     }),
   ],
+  base: "/myelectronics/",
 });
