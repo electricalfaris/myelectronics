a260fa72bc1dc9daa8547b99668ffa3aef011d4a
electricalfaris <electricalfaris@gmail.com>
2024-03-11 03:05:10 +0300

    sdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import "./App.css";
 import CardsData from "./data";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function Card({ title, image_url, desc, ref1, ref2 }) {
   return (
     <>
-      <div className="card">
-        <img src={image_url} className="card-img-top" alt={title} />
-        <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{desc}</p>
-          <div className="button-links">
-            <a href={ref1} className="btn btn-primary">
-              {ref1}
-            </a>
-            <a href={ref2} className="btn btn-warning">
-              {ref2}
-            </a>
+      <div className="col-12 col-md-6 col-lg-4 mb-4">
+        <div className="card">
+          <img className="card-img-top" src={image_url} alt="Card image cap" />
+          <div className="card-body">
+            <p className="card-text">{desc}</p>
+            <div className="links">
+              <a href={ref1} className="btn btn-primary">
+                {ref1}
+              </a>
+              <a href={ref2} className="btn btn-primary">
+                {ref2}
+              </a>
+            </div>
           </div>
         </div>
       </div>
@@ -27,18 +29,22 @@ function App() {
   return (
     <>
       <h1>My Electronics</h1>
+      <hr />
+
       <div className="container">
-        <div className="grid">
-          {CardsData.map((card) => (
-            <Card
-              key={card.id}
-              title={card.title}
-              image_url={card.image_url}
-              desc={card.desc}
-              ref1={card.ref1}
-              ref2={card.ref2}
-            />
-          ))}
+        <div class="row">
+          {CardsData.map((data) => {
+            return (
+              <Card
+                key={data.id}
+                title={data.title}
+                image_url={data.image_url}
+                desc={data.desc}
+                ref1={data.ref1}
+                ref2={data.ref2}
+              />
+            );
+          })}
         </div>
       </div>
     </>
